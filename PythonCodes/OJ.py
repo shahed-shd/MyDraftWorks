87b4b88e7eba3cdb8ec80cba63cb60254892f7e7
shahed-shd <shahed.shd777@gmail.com>
2017-11-21 21:21:10 +0600

    CF problem solving

--- PythonCodes/OJ.py
+++ PythonCodes/OJ.py
@@ -1,9 +1,9 @@
 #! /usr/bin/python3
 
 # ==================================================
-# Problem   :   Ugly Numbers
-# Run time  :   35 /35
-# Language  :   Python 3
+# Problem   :   894A - QAQ
+# Run time  :   0.062 sec.
+# Language  :   Python 3.6
 # ==================================================
 
 
@@ -11,21 +11,19 @@ def main():
     from sys import stdin, stdout
 
     # stdin = open("in.txt", "r")
+    # it = iter(map(int, stdin.read().split()))
 
-    it = iter(map(int, stdin.read().split()))
+    s = stdin.read()
 
-    n = next(it)
-    m = next(it)
+    tot_q_cnt = s.count('Q')
+    curr_q_cnt = 0
+    ans = 0
 
-    mn = min(n, m)
-    rem = n + m - 2 * mn
-    # ans = 0
-
-    if mn <= rem:
-        ans = mn
-    else:
-        ans = rem
-        ans += ((mn - rem) // 3) * 2
+    for x in s:
+    	if x == 'A':
+    		ans += curr_q_cnt * (tot_q_cnt - curr_q_cnt)
+    	elif x == 'Q':
+    		curr_q_cnt += 1
 
     stdout.write(str(ans) + '\n')
 
