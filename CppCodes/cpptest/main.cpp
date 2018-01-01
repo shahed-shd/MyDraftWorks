4adf079187a8721d298dc96aae77c270a0d09fa1
shahed-shd <shahed.shd777@gmail.com>
2018-01-02 00:19:32 +0600

    UVa problem solving

--- CppCodes/cpptest/main.cpp
+++ CppCodes/cpptest/main.cpp
@@ -1,6 +1,6 @@
 // ==================================================
-// Problem  :   10687 - Monitoring the Amazon
-// Run time :   0.020 sec.
+// Problem  :   318 - Domino Effect
+// Run time :   0.000 sec.
 // Language :   C++11
 // ==================================================
 
@@ -13,15 +13,16 @@ using namespace std;
 
 typedef     pair<int, int>      ii;
 
+
+const int MAXN = 500 + 3;
 const int INF = INT_MAX;
 
 vector<vector<ii> > adjList;
-
+int dist[MAXN];
 
 void dijkstra(int s, int n)
 {
     priority_queue<ii, vector<ii>, greater<ii> > pq;
-    int dist[n+3];
     fill(dist, dist+n+1, INF);
 
     pq.push(ii(0, s));
@@ -40,39 +41,43 @@ void dijkstra(int s, int n)
             }
         }
     }
+}
 
-    int mxDist1 = -1, mxDist2 = -1, node1, node2;
 
-    for(int i = 1; i <= n; ++i) {
-        if(mxDist1 <= dist[i]) {
-            mxDist2 = mxDist1, node2 = node1;
-            mxDist1 = dist[i], node1 = i;
-        }
-        else if(mxDist2 < dist[i]) {
-            mxDist2 = dist[i], node2 = i;
+void print_output(int n)
+{
+    double d = -1.0;
+    int node1 = -1, node2 = -1;
+
+    for(int u = 1; u <= n; ++u) {
+        if(dist[u] >= d)
+            d = dist[u], node1 = u, node2 = -1;
+
+        for(auto &pr : adjList[u]) {
+            int v = pr.first;
+            int w = pr.second;
+
+            double tmp = (dist[u] + dist[v] + w) / 2.0;
+
+            if(tmp > d)
+                d = tmp, node1 = u, node2 = v;
         }
     }
 
-    if(mxDist1 == mxDist2) {
-        for(auto &pr : adjList[node1]) {
-            if(pr.first == node2) {
-                printf("The last domino falls after %.1f seconds, between key dominoes %d and %d.\n", mxDist1+pr.second/2.0, node1, node2);
-                return;
-            }
-        }
-    }
-
-    printf("The last domino falls after %.1f seconds, at key domino %d.\n", double(mxDist1), node1);
+    if(node2 == -1)
+        printf("The last domino falls after %.1f seconds, at key domino %d.\n", d, node1);
+    else
+        printf("The last domino falls after %.1f seconds, between key dominoes %d and %d.\n", d, node1, node2);
 }
 
 
 int main()
 {
-    freopen("in.txt", "r", stdin);
+    //freopen("in.txt", "r", stdin);
 
-    int n, m;
+    int n, m, tc = 0;
 
-    while(scanf("%d %d", &n,  &m), n and m) {
+    while(scanf("%d %d", &n,  &m), n or m) {
         adjList.clear();
 
         adjList.resize(n+3);
@@ -85,7 +90,10 @@ int main()
             adjList[v].push_back(ii(u, w));
         }
 
+        printf("System #%d\n", ++tc);
         dijkstra(1, n);
+        print_output(n);
+        putchar('\n');
     }
 
     return 0;
