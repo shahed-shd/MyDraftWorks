21058a1e3f6d45e2b80a7b61e610d910b202f7c6
shahed-shd <shahed.shd777@gmail.com>
2018-03-26 03:25:38 +0600

    Python3 environment is ready to run and debug

---
+++ PythonCodes/vscode_workspace_py/main.py
@@ -0,0 +1,21 @@
+# ==================================================
+# Problem   :   
+# Run time  :    sec.
+# Language  :   Python 3.5
+# ==================================================
+
+
+def main():
+    # from sys import stdin, stdout
+
+    # stdin = open("in.txt", "r")
+    # it = iter(map(int, stdin.read().split()))
+    # ansL = []
+
+    print("Hello World!")
+
+    # stdout.write(''.join(ansL))
+
+
+if __name__ == '__main__':
+    main()
