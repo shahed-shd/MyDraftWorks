79aca8fedda3b90df134b3211111baa4a4c53c02
shahed-shd <shahed.shd777@gmail.com>
2018-04-09 03:24:18 +0600

    Have started learning flask

---
+++ Flask_test/vscode_workspace_flask_py/main.py
@@ -0,0 +1,14 @@
+
+
+def main():
+    # from sys import stdin, stdout
+
+    # stdin = open("in.txt", "r")
+    # it = iter(map(int, stdin.read().split()))
+    # ansL = []
+
+    # stdout.write(''.join(ansL))
+
+
+if __name__ == '__main__':
+    main()
