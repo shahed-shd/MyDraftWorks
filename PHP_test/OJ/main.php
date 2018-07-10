5a7ce9b0680df4b9c156916a5f2b37fe6ae2153b
Md. Shahedul Islam Shahed <shahed.shd777@gmail.com>
2018-07-10 18:06:40 +0600

    Code template for online judge problem solving with PHP.

---
+++ PHP_test/OJ/main.php
@@ -0,0 +1,22 @@
+<?php
+// ==================================================
+// Problem  :   ProblemNameGoesHere
+// Run time :   0.000 sec
+// Language :   PHP 7.0.12
+// ==================================================
+
+
+function main()
+{
+    # I/O streams.
+    define('INSTREAM', STDIN, FALSE);
+    define('OUTSTREAM', STDOUT, FALSE);
+    // define('INSTREAM', fopen("in.txt", "r"), FALSE);
+    // define('OUTSTREAM', fopen("out.txt", "w"), FALSE);
+
+
+}
+
+main();
+
+?>
