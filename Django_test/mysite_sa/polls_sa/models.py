526db1a537913d3b2f7fb9d9ca9437b5e8717c76
shahed-shd <shahed.shd777@gmail.com>
2018-10-11 19:34:27 +0600

    creating app going on.

---
+++ Django_test/mysite_sa/polls_sa/models.py
@@ -0,0 +1,45 @@
+import datetime
+
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+
+from django.conf import settings
+from django.utils import timezone
+
+
+engine = create_engine('sqlite:////' + settings.DATABASES['default']['NAME'])
+Base = declarative_base()
+
+
+class Question(Base):
+    __tablename__ = 'questions'
+
+    id = Column('id', Integer, primary_key=True)
+    question_text = Column('question_text', String)
+    pub_date = Column('pub_date', DateTime)
+
+    choices = relationship("Choice", back_populates='question')
+
+
+    def __repr__(self):
+        return "<Question(id={}, question_text={}, pub_date={})>".format(self.id, self.question_text, self.pub_date)
+
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+
+class Choice(Base):
+    __tablename__ = 'choices'
+
+    id = Column('id', Integer, primary_key=True)
+    choice_text = Column('choice_text', String)
+    votes = Column('votes', Integer)
+    question_id = Column('question_id', String, ForeignKey('questions.id'))
+
+    question = relationship('Question', back_populates='choices')
+
+    def __repr__(self):
+        return "<Choice(id={}, choice_text={}, votes={})>".format(self.id, self.question_text, self.pub_date)
