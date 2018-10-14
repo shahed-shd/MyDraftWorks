8732bcce34f351863a84bc9404fcc82bc98e46aa
shahed-shd <shahed.shd777@gmail.com>
2018-10-14 13:17:33 +0600

    voting feature and result show done.

--- Django_test/mysite_sa/polls_sa/views.py
+++ Django_test/mysite_sa/polls_sa/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 
-from .models import Question, session
+from .models import Question, Choice, session
 
 
 class IndexView(generic.ListView):
@@ -25,7 +25,37 @@ class DetailView(generic.DetailView):
         instance = session.query(Question).filter_by(id=x).one()
         return instance
 
-    # def get_queryset(self):
-        # '''Excludes any questions that aren't published yet.'''
-        # return session.query(Question).filter(Question.pub_date <= timezone.now())
-        # return session.query(Question).filter(Question.id==pk)
+
+class ResultsView(generic.DetailView):
+    context_object_name = 'object'
+    template_name = 'polls_sa/results.html'
+
+    def get_queryset(self):
+        return session.query(Question).all()
+
+    def get_object(self, *args, **kwargs):
+        x = self.kwargs.get('question_id')
+        instance = session.query(Question).filter_by(id=x).one()
+        return instance
+
+
+def vote(request, question_id):
+    question = session.query(Question).filter_by(id=question_id)
+
+    try:
+        choice_id = int(request.POST['choice'])
+        selected_choice = session.query(Choice).filter_by(question_id=question_id).filter_by(id=choice_id).one()
+    except:
+        # Redisplay the question voting form.
+        return render(request, 'polls_sa/detail.html', {
+            'object': question,
+            'error_message': "You didn't select a choice."
+        })
+    else:
+        selected_choice.votes += 1
+        session.commit()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+
+        return HttpResponseRedirect(reverse('polls_sa:results', args=(question_id,)))
