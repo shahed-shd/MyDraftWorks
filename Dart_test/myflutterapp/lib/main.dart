41492ca7ca1ce0a081ff2efda2797066b2a2dbda
Md. Shahedul Islam Shahed <shahed.shd777@gmail.com>
2019-09-11 20:04:17 +0600

    StatefulWidget implemented

--- Dart_test/myflutterapp/lib/main.dart
+++ Dart_test/myflutterapp/lib/main.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
+
 void main() => runApp(MyApp());
+
+
+class RandomWordsState extends State<RandomWords> {
+    @override
+    Widget build(BuildContext context) {
+        final wordPair = WordPair.random();
+        return Text(wordPair.asPascalCase);
+    }
+}
+
+
+class RandomWords extends StatefulWidget {
+    @override
+    RandomWordsState createState() => RandomWordsState();
+}
+
 
 class MyApp extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
-        final word_pair = WordPair.random();
+        // final word_pair = WordPair.random();
         return MaterialApp(
             title: 'Welcome to Flutter',
             home: Scaffold(
@@ -15,7 +32,8 @@ class MyApp extends StatelessWidget {
                 ),
                 body: Center(
                     // child: Text('Hello World'),
-                    child: Text(word_pair.asPascalCase),
+                    // child: Text(word_pair.asPascalCase),
+                    child: RandomWords(),
                 ),
             ),
         );
