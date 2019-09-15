374a89fd5c9cc2aad319da780aeac70127b8ef24
Md. Shahedul Islam Shahed <shahed.shd777@gmail.com>
2019-09-15 19:04:39 +0600

    Image asset used. 2.5 done

--- Dart_test/smartherd_flutter_app/lib/app_screens/home.dart
+++ Dart_test/smartherd_flutter_app/lib/app_screens/home.dart
@@ -68,9 +68,21 @@ class Home extends StatelessWidget {
                                 )),
                             ],
                         ),
+                        FlightImageAsset(),
                     ],
                 ),
             ),
         );
+    }
+}
+
+
+class FlightImageAsset extends StatelessWidget {
+
+    @override
+    Widget build(BuildContext context) {
+        AssetImage assetImage = AssetImage('images/flight.png');
+        Image image = Image(image: assetImage, width: 250.0, height: 250.0,);
+        return Container(child: image);
     }
 }
