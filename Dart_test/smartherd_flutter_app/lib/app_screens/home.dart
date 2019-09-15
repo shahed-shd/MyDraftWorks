5a7711f9f39037c1ef3130933d30e41a17282dd5
Md. Shahedul Islam Shahed <shahed.shd777@gmail.com>
2019-09-15 19:31:34 +0600

    RaisedButton and AlertDialog used. 2.6 done

--- Dart_test/smartherd_flutter_app/lib/app_screens/home.dart
+++ Dart_test/smartherd_flutter_app/lib/app_screens/home.dart
@@ -69,6 +69,7 @@ class Home extends StatelessWidget {
                             ],
                         ),
                         FlightImageAsset(),
+                        FlightBookButton(),
                     ],
                 ),
             ),
@@ -85,4 +86,44 @@ class FlightImageAsset extends StatelessWidget {
         Image image = Image(image: assetImage, width: 250.0, height: 250.0,);
         return Container(child: image);
     }
+}
+
+
+class FlightBookButton extends StatelessWidget {
+
+    @override
+    Widget build(BuildContext context) {
+        return Container(
+            margin: EdgeInsets.only(top: 30.0),
+            width: 250.0,
+            height: 50.0,
+            child: RaisedButton(
+                color: Colors.deepOrange,
+                child: Text(
+                    "Book Your Flight",
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                        fontFamily: 'Raleway',
+                        fontWeight: FontWeight.w700,
+                    ),
+                ),
+                elevation: 6.0,
+                onPressed: () => bookFlight(context),
+            ),
+        );
+    }
+
+    void bookFlight(BuildContext context) {
+        var alertDialogue = AlertDialog(
+            title: Text("Flight Booked Successfully"),
+            content: Text("Have a pleasant flight"),
+        );
+
+        showDialog(
+            context: context,
+            builder: (BuildContext context) => alertDialogue,
+        );
+    }
+
 }
