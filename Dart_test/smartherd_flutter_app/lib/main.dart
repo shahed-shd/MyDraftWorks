6bfd5d6ed0155e931d4fad529fddebc6d79ae13f
Md. Shahedul Islam Shahed <shahed.shd777@gmail.com>
2019-09-15 13:10:57 +0600

    Code organized, 1.5 done

--- Dart_test/smartherd_flutter_app/lib/main.dart
+++ Dart_test/smartherd_flutter_app/lib/main.dart
@@ -1,22 +1,20 @@
 import 'package:flutter/material.dart';
+import 'app_screens/first_screen.dart';
 
 
-void main() {
-    runApp(
-        MaterialApp(
-            title: 'Smartherd Flutter App',
-            home: Scaffold(
-                appBar: AppBar(title: Text('My First App Screen'),),
-                body: Material(
-                    color: Colors.lightBlueAccent,
-                    child: Center(
-                        child: Text(
-                            "Hello Flutter",
-                            textDirection: TextDirection.ltr,
-                        ),
-                    ),
-                ),
-            ),
-        )
-    );
+void main() => runApp(MyFlutterApp());
+
+
+class MyFlutterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+      return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Smartherd Flutter App',
+          home: Scaffold(
+              appBar: AppBar(title: Text('My First App Screen'),),
+              body: FirstScreen(),
+          ),
+      );
+  }
 }
