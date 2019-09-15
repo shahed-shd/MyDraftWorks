244c3402a2aa6a12fcef66dc8666249e4ed4e3c5
Md. Shahedul Islam Shahed <shahed.shd777@gmail.com>
2019-09-15 18:51:53 +0600

    Row and Column widget used. 2.4 done

--- Dart_test/smartherd_flutter_app/lib/app_screens/home.dart
+++ Dart_test/smartherd_flutter_app/lib/app_screens/home.dart
@@ -5,28 +5,72 @@ class Home extends StatelessWidget {
 
     @override
     Widget build(BuildContext context) {
-    return Center(
-        child: Container(
-            alignment: Alignment.center,
-            color: Colors.deepPurple,
-//            width: 200.0,
-//            height: 100.0,
-//            margin: EdgeInsets.all(35.0),
-//            padding: EdgeInsets.all(15.0),
-            child: Text(
-                "Flight",
-                textDirection: TextDirection.ltr,
-                style: TextStyle(
-                    decoration: TextDecoration.none,
-                    fontSize: 75.0,
-                    fontFamily: 'Raleway',
-                    fontWeight: FontWeight.w700,
-                    color: Colors.white,
+        return Center(
+            child: Container(
+                padding: EdgeInsets.only(left: 10.0, top: 40.0),
+                alignment: Alignment.center,
+                color: Colors.deepPurple,
+    //            width: 200.0,
+    //            height: 100.0,
+    //            margin: EdgeInsets.all(35.0),
+    //            padding: EdgeInsets.all(15.0),
+                child: Column(
+                    children: <Widget>[
+                        Row(
+                            children: <Widget>[
+                                Expanded(child: Text(
+                                    "Spice Jet",
+                                    textDirection: TextDirection.ltr,
+                                    style: TextStyle(
+                                        decoration: TextDecoration.none,
+                                        fontSize: 35.0,
+                                        fontFamily: 'Raleway',
+                                        fontWeight: FontWeight.w700,
+                                        color: Colors.white,
+                                    ),
+                                )),
+                                Expanded(child: Text(
+                                    "From Mumbai to Bangalore via New Delhi",
+                                    textDirection: TextDirection.ltr,
+                                    style: TextStyle(
+                                        decoration: TextDecoration.none,
+                                        fontSize: 20.0,
+                                        fontFamily: 'Raleway',
+                                        fontWeight: FontWeight.w700,
+                                        color: Colors.white,
+                                    ),
+                                )),
+                            ],
+                        ),
+                        Row(
+                            children: <Widget>[
+                                Expanded(child: Text(
+                                    "Air India",
+                                    textDirection: TextDirection.ltr,
+                                    style: TextStyle(
+                                        decoration: TextDecoration.none,
+                                        fontSize: 35.0,
+                                        fontFamily: 'Raleway',
+                                        fontWeight: FontWeight.w700,
+                                        color: Colors.white,
+                                    ),
+                                )),
+                                Expanded(child: Text(
+                                    "From Jaipur to Goa",
+                                    textDirection: TextDirection.ltr,
+                                    style: TextStyle(
+                                        decoration: TextDecoration.none,
+                                        fontSize: 20.0,
+                                        fontFamily: 'Raleway',
+                                        fontWeight: FontWeight.w700,
+                                        color: Colors.white,
+                                    ),
+                                )),
+                            ],
+                        ),
+                    ],
                 ),
             ),
-        ),
-    );
-
-
-  }
+        );
+    }
 }
