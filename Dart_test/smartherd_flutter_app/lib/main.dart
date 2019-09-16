169bb93ceb7f9caae0460b128c5f55bc920cdf0e
Md. Shahedul Islam Shahed <shahed.shd777@gmail.com>
2019-09-16 15:55:02 +0600

    UI designed for Simple Interest Calculator. 3.5 done

--- Dart_test/smartherd_flutter_app/lib/main.dart
+++ Dart_test/smartherd_flutter_app/lib/main.dart
@@ -5,77 +5,148 @@ import 'app_screens/home.dart';
 void main() {
     runApp(
         MaterialApp(
-            title: "Stateful App Example",
-            home: FavouriteCity(),
+            title: 'Simple Interest Calculator APP',
+            home: SIForm(),
         )
     );
 }
 
 
-class FavouriteCity extends StatefulWidget {
+class SIForm extends StatefulWidget {
 
     @override
     State<StatefulWidget> createState() {
-        return _FavouriteCityState();
+        return _SIFormState();
     }
 }
 
 
-class _FavouriteCityState extends State<FavouriteCity> {
-    String nameCity = "";
-    static var _currencies = ['BDT', 'Rupees', 'Dollars', 'Pounds', 'Others'];
-    var _currentItemSelected = _currencies[0];
+class _SIFormState extends State<SIForm> {
+    var _currencies = ['BDT', 'Rupees', 'Dollars', 'Pounds'];
+    final _minimumPadding = 5.0;
 
     @override
     Widget build(BuildContext context) {
         return Scaffold(
+//            resizeToAvoidBottomPadding: false,
             appBar: AppBar(
-                title: Text("Stateful App Example"),
+                title: Text('Simple Interest Calculator'),
             ),
             body: Container(
-                margin: EdgeInsets.all(20.0),
-                child: Column(
+//                margin: EdgeInsets.all(_minimumPadding * 2),
+                child: ListView(
                     children: <Widget>[
-                        TextField(
-                            onSubmitted: (String userInput) {
-                                setState(() {
-                                    nameCity = userInput;
-                                });
-                            },
-                        ),
+                        getImageAsset(),
+
                         Padding(
-                            padding: EdgeInsets.all(30.0),
-                            child: Text(
-                                "Your bext city is $nameCity",
-                                style: TextStyle(fontSize: 20.0),
+                            padding: EdgeInsets.only(top: _minimumPadding, bottom: _minimumPadding),
+                            child: TextField(
+                                keyboardType: TextInputType.number,
+                                decoration: InputDecoration(
+                                    labelText: 'Principal',
+                                    hintText: 'Enter Principal e.g. 12000',
+                                    border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(5.0),
+                                    ),
+                                ),
                             ),
                         ),
-                        DropdownButton<String>(
-                            items: _currencies.map((String dropDownStringItem) {
-                                return DropdownMenuItem<String>(
-                                    value: dropDownStringItem,
-                                    child: Text(dropDownStringItem),
-                                );
-                            }).toList(),
-                            value: _currentItemSelected,
-                            onChanged: (String newValueSelected) {
-                                _onDropDownItemSelected(newValueSelected);
-                            },
+
+                        Padding(
+                            padding: EdgeInsets.only(top: _minimumPadding, bottom: _minimumPadding),
+                            child: TextField(
+                                keyboardType: TextInputType.number,
+                                decoration: InputDecoration(
+                                    labelText: 'Rate of Interest',
+                                    hintText: 'In percent',
+                                    border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(5.0),
+                                    ),
+                                ),
+                            ),
                         ),
+
+                        Padding(
+                            padding: EdgeInsets.only(top: _minimumPadding, bottom: _minimumPadding),
+                            child: Row(
+                                children: <Widget>[
+                                    Expanded(
+                                        child: TextField(
+                                            keyboardType: TextInputType.number,
+                                            decoration: InputDecoration(
+                                                labelText: 'Term',
+                                                hintText: 'Time in years',
+                                                border: OutlineInputBorder(
+                                                    borderRadius: BorderRadius.circular(5.0),
+                                                ),
+                                            ),
+                                        ),
+                                    ),
+
+                                    Container(width: _minimumPadding * 5),
+
+                                    Expanded(
+                                        child: DropdownButton<String>(
+                                            items: _currencies.map((String value) {
+                                                return DropdownMenuItem(
+                                                    value: value,
+                                                    child: Text(value),
+                                                );
+                                            }).toList(),
+
+                                            value: 'BDT',
+
+                                            onChanged: (String newValueSelected) {
+
+                                            },
+                                        ),
+                                    ),
+                                ],
+                            ),
+                        ),
+
+                        Padding(
+                            padding: EdgeInsets.only(top: _minimumPadding, bottom: _minimumPadding),
+                            child: Row(
+                                children: <Widget>[
+                                    Expanded(
+                                        child: RaisedButton(
+                                            child: Text('Calculate'),
+                                            onPressed: () {
+
+                                            },
+                                        ),
+                                    ),
+
+                                    Expanded(
+                                        child: RaisedButton(
+                                            child: Text('Reset'),
+                                            onPressed: () {
+
+                                            },
+                                        ),
+                                    )
+                                ],
+                            ),
+                        ),
+
+                        Padding(
+                            padding: EdgeInsets.only(top: _minimumPadding, bottom: _minimumPadding),
+                            child: Text("ToDo Text"),
+                        )
+
                     ],
                 ),
             ),
         );
     }
 
-    void _onDropDownItemSelected(String newValueSelected) {
-        setState(() {
-            this._currentItemSelected = newValueSelected;
-        });
+    Widget getImageAsset() {
+        AssetImage assetImage = AssetImage('images/money.png');
+        Image image = Image(image: assetImage, width: 125.0, height: 125);
+        return Container(child: image, margin: EdgeInsets.all(_minimumPadding * 10));
     }
 }
-
-
 
 
 //void main() {
