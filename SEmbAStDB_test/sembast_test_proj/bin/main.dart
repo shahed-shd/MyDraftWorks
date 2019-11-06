34ed6e8e81ea5d42386628e12fb395e50c049919
Md. Shahedul Islam Shahed <shahed.shd777@gmail.com>
2019-11-06 19:26:53 +0600

    sembast db tested

--- SEmbAStDB_test/sembast_test_proj/bin/main.dart
+++ SEmbAStDB_test/sembast_test_proj/bin/main.dart
@@ -9,5 +9,26 @@ void main() async {
   DatabaseFactory dbFactory = databaseFactoryIo;
   Database db = await dbFactory.openDatabase(dbPath);
 
-  print(db.runtimeType);
+  // Reading and writing
+  var store = StoreRef.main();
+  await store.record('title').put(db, 'Simple application');
+  await store.record('version').put(db, 10);
+  await store.record('settings').put(db, {'offline': true});
+  await store.record('version').delete(db);
+
+  var intMapStore = intMapStoreFactory.store('animals');  // int map
+  await db.transaction((txn) async {
+    await intMapStore.add(txn, {'name': 'fish'});
+    await intMapStore.add(txn, {'name': 'cat'});
+  });
+
+  var storeTyped = StoreRef<String, String>.main();   // strong mode to make database access less error prone
+  await storeTyped.record('username').put(db, 'my_username');
+  await storeTyped.record('url').put(db, 'my_url');
+
+  var url = await storeTyped.record('url').get(db);
+  var username = await storeTyped.record('username').get(db);
+  print("$url, $username");
+
+  await db.close();
 }
