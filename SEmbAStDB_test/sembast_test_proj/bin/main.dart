00e91ec170c5bbfda28c62a52e168a8e5c786797
Md. Shahedul Islam Shahed <shahed.shd777@gmail.com>
2019-11-06 16:07:22 +0600

    db opened

--- SEmbAStDB_test/sembast_test_proj/bin/main.dart
+++ SEmbAStDB_test/sembast_test_proj/bin/main.dart
@@ -1,5 +1,13 @@
 import 'package:sembast/sembast.dart';
+import 'package:sembast/sembast_io.dart';
 
-void main() {
+void main() async {
   print("Hello SEmbASt DB");
+
+  // Opening a database
+  String dbPath = 'sample.db';
+  DatabaseFactory dbFactory = databaseFactoryIo;
+  Database db = await dbFactory.openDatabase(dbPath);
+
+  print(db.runtimeType);
 }
