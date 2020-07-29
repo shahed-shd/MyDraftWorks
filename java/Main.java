4d615d46861a826b11fb2498d60fda124463cd40
Md. Shahedul Islam Shahed <shahed.shd777@gmail.com>
2020-07-29 12:39:55 +0600

    Hello Java program modified

--- java/Main.java
+++ java/Main.java
@@ -1,5 +1,9 @@
+import java.io.PrintStream;
+
 public class Main {
-    public static void main(final String args[]) {
-        System.out.println("Hello Java");
+    public static final PrintStream printStream = System.out;
+
+    public static void main(final String[] args) {
+        printStream.println("Hello Java");
     }
 }
