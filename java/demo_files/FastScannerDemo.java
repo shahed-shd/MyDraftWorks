bda774166d7a705d7e70198a49867dd9611961ae
Md. Shahedul Islam Shahed <shahed.shd777@gmail.com>
2020-08-12 03:41:56 +0600

    Java FastScanner demo program added

---
+++ java/demo_files/FastScannerDemo.java
@@ -0,0 +1,95 @@
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class FastScannerDemo {
+    public static void main(String[] args) {
+        FastScanner sc = new FastScanner();
+        PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+
+        int nextIntValue = sc.nextInt();
+        long nextLongValue = sc.nextLong();
+        double nextDoubleValue = sc.nextDouble();
+        String nextTokenValue = sc.nextToken();
+        String nextLineValue = sc.nextLine();
+        String readLineValue = sc.readLine();
+
+        out.print(nextIntValue);
+        out.print('\n');
+        out.println(nextLongValue);
+        out.println(nextDoubleValue);
+
+        out.append(nextTokenValue);
+        out.append('\n');
+        out.append(nextLineValue);
+        out.append("\n");
+        out.append(readLineValue);
+
+        out.flush();
+        out.close();
+    }
+}
+
+class FastScanner {
+    private BufferedReader br;
+    private StringTokenizer st;
+
+    public FastScanner(String fileName) {
+        try {
+            br = new BufferedReader(new FileReader(fileName));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public FastScanner() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    String readLine() {
+        String str = "";
+        try {
+            str = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
+
+    public String nextToken() {
+        while (st == null || !st.hasMoreElements()) {
+            st = new StringTokenizer(readLine());
+        }
+        return st.nextToken();
+    }
+
+    public String nextLine() {
+        String str = "";
+
+        if (st != null && st.hasMoreElements()) {
+            str = st.nextToken("\n\r\f");
+        } else {
+            str = readLine();
+        }
+
+        return str;
+    }
+
+    public int nextInt() {
+        return Integer.parseInt(nextToken());
+    }
+
+    public long nextLong() {
+        return Long.parseLong(nextToken());
+    }
+
+    public double nextDouble() {
+        return Double.parseDouble(nextToken());
+    }
+
+}
