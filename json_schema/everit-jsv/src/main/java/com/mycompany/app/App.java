c75ad26f02d373323f9c930b67860ee4b9e5f34d
Md. Shahedul Islam Shahed <shahed.shd777@gmail.com>
2021-02-12 22:53:31 +0600

    Java program added to validate instance using json schema

--- json_schema/everit-jsv/src/main/java/com/mycompany/app/App.java
+++ json_schema/everit-jsv/src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,46 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import org.everit.json.schema.Schema;
+import org.everit.json.schema.ValidationException;
+import org.everit.json.schema.loader.SchemaLoader;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+public class App {
+    static PrintStream printStream = System.out;
+
+    public static void main(final String[] args) {
+        // ToDo: Need to assign absolute path of the files.
+        final String schemaPath = "absolute_path_of_the_demo_schema.json";
+        final String instancePath = "absolute_path_of_the_demo_instance.json";
+
+        validate(schemaPath, instancePath);
+    }
+
+    public static void validate(final String schemaPath, final String instancePath) {
+
+        printStream.println("SCHEMA: " + schemaPath);
+        printStream.println("INSTANCE: " + instancePath);
+        printStream.println("Validating . . .");
+
+        try (InputStream schemaStream = new FileInputStream(schemaPath);
+                InputStream instanceStream = new FileInputStream(instancePath)) {
+
+            JSONObject rawSchema = new JSONObject(new JSONTokener(schemaStream));
+            Schema schema = SchemaLoader.load(rawSchema);
+
+            JSONObject instance = new JSONObject(new JSONTokener(instanceStream));
+            schema.validate(instance);
+            printStream.println("Validation successful");
+        } catch (IOException | ValidationException e) {
+            e.printStackTrace(printStream);
+        } finally {
+            printStream.println("Done");
+        }
     }
 }
